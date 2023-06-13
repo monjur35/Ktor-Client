74afbf87f66de9f659be77a4083915ab54a480dd
monjurCEL <monjur@celimited.com>
2023-06-13 14:58:13 +0600

    ktor setup

--- app/src/main/java/com/example/ktorclient/HomeFragment.kt
+++ app/src/main/java/com/example/ktorclient/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
---
+++ app/src/main/java/com/example/ktorclient/netWork/KtorClient.kt
@@ -0,0 +1,64 @@
+package com.example.ktorclient.netWork
+
+
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
+
+
+class KtorClient @Inject constructor() {
+
+
+    private val getHttpClient = HttpClient(CIO) {
+
+        install(ContentNegotiation) {
+
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+            engine {
+                requestTimeout = TIME_OUT.toLong()
+            }
+        }
+
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Log.v("Logger Ktor =>", message)
+                }
+
+            }
+            level = LogLevel.ALL
+        }
+
+        install(ResponseObserver) {
+            onResponse { response ->
+                Log.d("TAG_HTTP_STATUS_LOGGER", "${response.status.value}")
+            }
+        }
+
+        install(DefaultRequest) {
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+        }
+    }
+
+
+    companion object {
+        private const val TIME_OUT = 10_000
+        private const val TAG_KTOR_LOGGER = "ktor_logger:"
+        private const val TAG_HTTP_STATUS_LOGGER = "http_status:"
+    }
+}
